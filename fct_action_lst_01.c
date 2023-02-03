b4c7db9dfb0fee83fbfe4dc6c98f2c60dfc605ac
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-02-03 12:34:28 +0100

    safety push

--- fct_action_lst_01.c
+++ fct_action_lst_01.c
@@ -6,7 +6,7 @@
 /*   By: fcoindre <fcoindre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 21:47:26 by fcoindre          #+#    #+#             */
-/*   Updated: 2023/02/02 21:48:14 by fcoindre         ###   ########.fr       */
+/*   Updated: 2023/02/03 12:25:08 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,46 +16,48 @@ void	ft_reverse_rotate_a(t_node **ptr_first_node_a)
 {
 	t_node	*first_node_a;
 
-    if (*ptr_first_node_a != NULL && ft_size_lst(ptr_first_node_a) > 1)
-    {
-        first_node_a = ft_lst_del_first(ptr_first_node_a);
-        ft_add_node(ptr_first_node_a, first_node_a);
-        write(1, "rra\n", 4);
-    }
+	if (*ptr_first_node_a != NULL && ft_size_lst(ptr_first_node_a) > 1)
+	{
+		first_node_a = ft_lst_del_first(ptr_first_node_a);
+		ft_add_node(ptr_first_node_a, first_node_a);
+		write(1, "rra\n", 4);
+	}
 }
 
 void	ft_reverse_rotate_b(t_node **ptr_first_node_b)
 {
 	t_node	*first_node_b;
 
-    if (*ptr_first_node_b != NULL && ft_size_lst(ptr_first_node_b) > 1)
-    {
-        first_node_b = ft_lst_del_first(ptr_first_node_b);
-        ft_add_node(ptr_first_node_b, first_node_b);
-        write(1, "rrb\n", 4);
-    }
+	if (*ptr_first_node_b != NULL && ft_size_lst(ptr_first_node_b) > 1)
+	{
+		first_node_b = ft_lst_del_first(ptr_first_node_b);
+		ft_add_node(ptr_first_node_b, first_node_b);
+		write(1, "rrb\n", 4);
+	}
 }
 
-void ft_push_b(t_node **ptr_first_node_a, t_node **ptr_first_node_b)
+void	ft_push_b(t_node **ptr_first_node_a, t_node **ptr_first_node_b)
 {
-    t_node  *last_node_a;
+	t_node	*last_node_a;
 
-    if (ptr_first_node_a != NULL && ptr_first_node_b != NULL && ft_size_lst(ptr_first_node_a) > 0)
-    {
-        last_node_a = ft_lst_del_last(ptr_first_node_a);
-        ft_add_node(ptr_first_node_b, last_node_a);
-        write(1,"pb\n",3);
-    }
+	if (ptr_first_node_a != NULL && ptr_first_node_b != NULL
+		&& ft_size_lst(ptr_first_node_a) > 0)
+	{
+		last_node_a = ft_lst_del_last(ptr_first_node_a);
+		ft_add_node(ptr_first_node_b, last_node_a);
+		write(1, "pb\n", 3);
+	}
 }
 
-void ft_push_a(t_node **ptr_first_node_a, t_node **ptr_first_node_b)
+void	ft_push_a(t_node **ptr_first_node_a, t_node **ptr_first_node_b)
 {
-    t_node  *last_node_b;
+	t_node	*last_node_b;
 
-    if (ptr_first_node_a != NULL && ptr_first_node_b != NULL && ft_size_lst(ptr_first_node_b) > 0)
-    {
-        last_node_b = ft_lst_del_last(ptr_first_node_b);
-        ft_add_node(ptr_first_node_a, last_node_b);
-        write(1,"pa\n",3);
-    }
+	if (ptr_first_node_a != NULL && ptr_first_node_b != NULL
+		&& ft_size_lst(ptr_first_node_b) > 0)
+	{
+		last_node_b = ft_lst_del_last(ptr_first_node_b);
+		ft_add_node(ptr_first_node_a, last_node_b);
+		write(1, "pa\n", 3);
+	}
 }
--- fct_big_sort_tools.c
+++ fct_big_sort_tools.c
@@ -6,116 +6,116 @@
 /*   By: fcoindre <fcoindre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 15:25:18 by fcoindre          #+#    #+#             */
-/*   Updated: 2023/02/02 23:04:04 by fcoindre         ###   ########.fr       */
+/*   Updated: 2023/02/03 12:31:04 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int ft_srch_from_bottom_a(t_node **ptr_first_node, int ind_min, int ind_max)
+int	ft_srch_from_bottom_a(t_node **ptr_first_node, int ind_min, int ind_max)
 {
-    int     i;
-    t_node  *current_node;
-    
-    i = 1;
-    current_node = *ptr_first_node;
-    if (current_node != NULL && ind_min < ind_max)
-    {
-        while (current_node != NULL)
-        {
-            if (current_node->ind >= ind_min && current_node->ind <= ind_max)
-            {
-                return (i);
-            }
-            i++;
-            current_node = current_node->next;
-        }
-    }
-    return (-1);
+	int		i;
+	t_node	*current_node;
+
+	i = 1;
+	current_node = *ptr_first_node;
+	if (current_node != NULL && ind_min < ind_max)
+	{
+		while (current_node != NULL)
+		{
+			if (current_node->ind >= ind_min && current_node->ind <= ind_max)
+			{
+				return (i);
+			}
+			i++;
+			current_node = current_node->next;
+		}
+	}
+	return (-1);
 }
 
-int ft_srch_from_top_a(t_node **ptr_first_node, int ind_min, int ind_max)
+int	ft_srch_from_top_a(t_node **ptr_first_node, int ind_min, int ind_max)
 {
-    int     i;
-    t_node  *current_node;
-    
-    i = 0;
-    current_node = ft_last_node(ptr_first_node);
-    if (current_node != NULL && ind_min < ind_max)
-    {
-        while (current_node != NULL)
-        {
-            if (current_node->ind >= ind_min && current_node->ind <= ind_max)
-            {
-                return (i);
-            }
-            i++;
-            current_node = current_node->previous;
-        }
-    }
-    return (-1);
+	int		i;
+	t_node	*current_node;
+
+	i = 0;
+	current_node = ft_last_node(ptr_first_node);
+	if (current_node != NULL && ind_min < ind_max)
+	{
+		while (current_node != NULL)
+		{
+			if (current_node->ind >= ind_min && current_node->ind <= ind_max)
+			{
+				return (i);
+			}
+			i++;
+			current_node = current_node->previous;
+		}
+	}
+	return (-1);
 }
 
-int ft_srch_from_bottom_b(t_node **ptr_first_node, int ind_value)
+int	ft_srch_from_bottom_b(t_node **ptr_first_node, int ind_value)
 {
-    int     i;
-    t_node  *current_node;
-    
-    i = 1;
-    current_node = *ptr_first_node;
-    if (current_node != NULL && ind_value > 0)
-    {
-        while (current_node != NULL)
-        {
-            if (current_node->ind == ind_value)
-            {
-                return (i);
-            }
-            i++;
-            current_node = current_node->next;
-        }
-    }
-    return (-1);
+	int		i;
+	t_node	*current_node;
+
+	i = 1;
+	current_node = *ptr_first_node;
+	if (current_node != NULL && ind_value > 0)
+	{
+		while (current_node != NULL)
+		{
+			if (current_node->ind == ind_value)
+			{
+				return (i);
+			}
+			i++;
+			current_node = current_node->next;
+		}
+	}
+	return (-1);
 }
 
-int ft_srch_from_top_b(t_node **ptr_first_node, int ind_value)
+int	ft_srch_from_top_b(t_node **ptr_first_node, int ind_value)
 {
-    int     i;
-    t_node  *current_node;
-    
-    i = 0;
-    current_node = ft_last_node(ptr_first_node);
-    if (current_node != NULL && ind_value > 0)
-    {
-        while (current_node != NULL)
-        {
-            if (current_node->ind == ind_value)
-            {
-                return (i);
-            }
-            i++;
-            current_node = current_node->previous;
-        }
-    }
-    return (-1);
+	int		i;
+	t_node	*current_node;
+
+	i = 0;
+	current_node = ft_last_node(ptr_first_node);
+	if (current_node != NULL && ind_value > 0)
+	{
+		while (current_node != NULL)
+		{
+			if (current_node->ind == ind_value)
+			{
+				return (i);
+			}
+			i++;
+			current_node = current_node->previous;
+		}
+	}
+	return (-1);
 }
 
-int ft_calculate_nb_chunk(int tablen)
+int	ft_calculate_nb_chunk(int tablen)
 {
-    if (tablen > 5 && tablen < 21)
-    {
-        return (2);
-    }
-    if (tablen == 100)
-    {
-        return (5);
-    }
-    else if (tablen == 500)
-    {
-        return (12);
-    }
-    else
-    {
-        return (((tablen * 7) / 400) + 3);
-    }
+	if (tablen > 5 && tablen < 21)
+	{
+		return (2);
+	}
+	if (tablen == 100)
+	{
+		return (5);
+	}
+	else if (tablen == 500)
+	{
+		return (12);
+	}
+	else
+	{
+		return (((tablen * 7) / 400) + 3);
+	}
 }
